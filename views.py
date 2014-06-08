4594aa8bccbb215484917e969b8a0b967cb347a8
andrewrod33 <andrewrodriguez2012@gmail.com>
2014-06-08 08:05:29 -0700

    Added URL Shortening to this app

--- views.py
+++ views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from library_app.models import Books
 from library_app.forms import BookForm
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts  import render_to_response,redirect
 from utils import get_query
-
+import requests
+from django.views.decorators.http import require_http_methods
 
 def home(request):
     books = Books.objects.all().order_by('-id')[:18]
-    data={'books':books}
+    data = {'books':books}
     return render_to_response('home.html', data)
 
 
@@ -44,4 +45,25 @@ def search(request):
 
     return render_to_response('search_results.html',
                           { 'query_string': query_string, 'found_entries': found_entries })
+
+def shorten(request):
+
+    return render_to_response('url.html')
+
+def getUrl(request):
+    query_string = ''
+    found_entries = None
+    data = {}
+    if ('url' in request.POST) and request.POST['url'].strip():
+        query_string = request.POST['url']
+
+    r = requests.get('https://api-ssl.bitly.com/v3/shorten?access_token=9a42cbf4c6c7c72121f0b1d64fa32502d5d1ab67&longUrl=' + query_string)
+
+    j = r.json()
+    urls = j['data']['url']
+
+    print r.content
+
+    return render_to_response('shorten.html',
+                          { 'query_string': query_string,'data':urls}, )
 
